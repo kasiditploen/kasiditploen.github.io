e66a7d7e0eec40ccb18f35d3cf2c83196dec791b
u6016901 <u6016901@au.edu>
2021-12-21 20:32:09 +0700

    Updated

--- pages/curriculum.tsx
+++ pages/curriculum.tsx
@@ -15,7 +15,7 @@ export default function Home({ courseData }) {
       <Head>
         <title> VMS:Curriculum</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="This is the curriculum page of VMS Website. The new and fresh VMS website from Space-Dust team using Next.js, a JavaScript framework bringing the best performance and security in both world. Next.js is widely used by the biggest and most popular companies all over the world like Netflix, Uber, Starbucks, or Twitch. It’s also considered as one of the fastest-growing React frameworks, perfect to work with static sites – which was the hottest topic in the web development world lately. It doesn’t matter if you are planning to build a huge and demanding app to serve millions of users, nor if you are a growing web shop on Shopify. In both cases, you can use the advantages of modern web technology to make your business more efficient online. Uplift your page speed, SEO, and User Experience, and remember that technologies such as Next.js are making the web a better, cleaner, and more user-centric place. And that will always be favorable, not only by Google but, most importantly, by users"></meta>
+        <meta name="description" content="This is the curriculum page of VMS Website. The new and fresh VMS website from Space-Dust team using Nextjs, a JavaScript framework bringing the best performance and security in both world. Next.js is widely used by the biggest and most popular companies all over the world like Netflix, Uber, Starbucks, or Twitch."></meta>
       </Head>
 
       <Navbar title={'Curriculum'} transparent={true} />
